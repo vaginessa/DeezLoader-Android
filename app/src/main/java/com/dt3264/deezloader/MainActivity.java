3f5caeaeaf2974b5963190e65811e93ea51b96d7
Nick Filmer <nick80835@gmail.com>
2018-09-15 13:08:51 -0400

    2.1.8 Beta

--- app/src/main/java/com/dt3264/deezloader/MainActivity.java
+++ app/src/main/java/com/dt3264/deezloader/MainActivity.java
@@ -60,7 +60,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * reloadNodeAppData when true, updates the node folder in the phone on each start
      * */
-    final boolean reloadNodeAppData = false;
+    final boolean reloadNodeAppData = true;
     final String actualVersion = "2.1.7";
     final String url = "http://localhost:1730";
     final String telegramUrl = "https://t.me/joinchat/Ed1JxEfoci-dp-BWGRdVLg";
