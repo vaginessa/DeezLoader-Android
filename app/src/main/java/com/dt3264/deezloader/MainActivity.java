ae13282f63c2282038ff40783a7f42a4e3a12480
Nick Filmer <nick80835@gmail.com>
2018-09-15 18:12:29 -0400

    2.1.8 Release

--- app/src/main/java/com/dt3264/deezloader/MainActivity.java
+++ app/src/main/java/com/dt3264/deezloader/MainActivity.java
@@ -56,12 +56,12 @@ public class MainActivity extends AppCompatActivity {
      * their last update and if it was a previous version, it updates the files
      * easy peasy ggg
      */
-    final int actualCompileNumber = 217;
+    final int actualCompileNumber = 218;
     /**
      * reloadNodeAppData when true, updates the node folder in the phone on each start
      * */
-    final boolean reloadNodeAppData = true;
-    final String actualVersion = "2.1.7";
+    final boolean reloadNodeAppData = false;
+    final String actualVersion = "2.1.8";
     final String url = "http://localhost:1730";
     final String telegramUrl = "https://t.me/joinchat/Ed1JxEfoci-dp-BWGRdVLg";
     int lastCompile;
